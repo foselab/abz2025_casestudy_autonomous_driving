6d5bf59258214d05374cc074f89d8056a24ac630
Silvia Bonfanti <silvia.bonfanti@unibg.it>
2025-01-17 12:25:51 +0100

    Update SafetyEnforcer.asm

--- asmeta spec/SafetyEnforcer.asm
+++ asmeta spec/SafetyEnforcer.asm
@@ -8,46 +8,88 @@ import StandardLibrary
 signature:
 	enum domain Actions={FASTER, SLOWER, IDLE, LANE_LEFT, LANE_RIGHT}
 	monitored inputAction: Actions
-	monitored v_r: Real //Absolute speed of the controlled vehicle
-	monitored v_f: Real //Absolute speed of the front vehicle
+	monitored v_self: Real //Absolute speed of the controlled vehicle
+	monitored v_front: Real //Absolute speed of the front vehicle
 	
-	monitored x_r: Real //Absolute position of the controlled vehicle
-	monitored x_f: Real //Absolute position of the front vehicle
+	monitored x_self: Real //Absolute position of the controlled vehicle
+	monitored x_front: Real //Absolute position of the front vehicle
 	
 	//monitored le osservazioni per le regole di enforcement? 
 	
-	controlled randomAction: Actions
-	controlled sameAction: Actions
 	out outAction: Actions
 	
-	static a_max: Real //Maximum acceleration of rear vehicle before breaking
-	static b_min: Real 
-	static b_max: Real
-	static resp_time: Real
+	controlled a_max: Real //Maximum acceleration of rear vehicle before breaking: m/s^2
+	controlled b_min: Real 
+	controlled b_max: Real
+	controlled resp_time: Real
 	
-	derived dRSS: Real //Safety distance
+	static v_max: Real // m/s
+	
+	static gofast_perc: Real
+	
+	derived dRSS: Prod(Real,Real)-> Real //Safety distance
 	
 definitions:
-
-	//function dRSS= max(2.3,5.2)
-	function dRSS= (v_r*resp_time) + (0.5 *a_max * pwr(resp_time,2)) + (pwr((v_r+resp_time*a_max),2)/(2*b_min)) - (pwr(v_f,2)/(2*b_max))
-	//function dRSS = max(0.0, ())
 	
+	function v_max = 40.0
+	function gofast_perc = 1.7
+	
+	function dRSS ($v_r in Real, $v_f in Real) = max(0.0, (($v_r*resp_time) + 
+	(0.5 *a_max * pwr(resp_time,2.0)) + 
+	(pwr(($v_r+resp_time*a_max),2.0)/(2.0*b_min)) - 
+	(pwr($v_f,2.0)/(2.0*b_max))))
 
-	main rule r_Main =
-	if ((x_f-x_r)>dRSS) then
-		if (inputAction != IDLE) then
-			outAction := IDLE
-		endif
-	endif
-	/*	par
-			choose $a in Actions with true
+	
+	
+	macro rule r_randomAction=
+		choose $a in Actions with true
 				do
-					randomAction := $a
-			sameAction := inputAction
-		endpar*/
+					outAction := $a
+	
+	// Keep current speed - no risk of collision	
+	macro rule r_Hold = 
+		if ((x_front-x_self)>dRSS(v_self,v_front) and (x_front-x_self)<=(dRSS(v_self,v_front)*gofast_perc)) then 
+			if (inputAction != IDLE) then
+				outAction := IDLE
+			endif
+		endif
+	
+	// Distance from front vehicle lower than safe distance: break
+	macro rule r_unsafeDistance = 
+		if ((x_front-x_self)<=dRSS(v_self,v_front)) then 
+			if (inputAction != SLOWER) then
+				outAction := SLOWER
+			endif
+		endif
+	
+	// Rear vehicle very far: increase speed	
+	macro rule r_goFast = 
+		if ((x_front-x_self)>(dRSS(v_self,v_front)*gofast_perc)) then 
+			if (inputAction != FASTER) then
+				outAction := FASTER
+			endif
+		endif
+		
+	main rule r_Main =
+		par
+			r_Hold[]
+			r_unsafeDistance[]
+			r_goFast[]
+			/*if isUndef(outAction) then
+				outAction := inputAction
+			endif*/
+		endpar
+	
 
 	//Esempi di enforcement rule 
 	// se distanza da front  d_f > d_min and agent action diversa da IDLE then IDLE
 	// se  d_f <= d_min and action diversa da SLOWER then SLOWER and alert
         // se distanza da front  d_f >> d_min and agent action diversa da FASTER then FASTER
+        
+   default init s0:
+   function a_max = 5.0
+   function b_max = 5.0
+   function b_min = 3.0
+   function resp_time = 1.0
+   
+   
